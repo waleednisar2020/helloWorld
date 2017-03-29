87197165f86f215ba99ff35051baf6e12d032249
Waleed Nisar <waleed24@hotmail.com>
2017-03-29 12:37:10 -0400

    progress on loop

--- practiceOne.py
+++ practiceOne.py
@@ -1,12 +1,23 @@
+# userNumber = input("Tell a whole number between 1 and 10: ")
+# userNumber = int(userNumber)
+# print(userNumber)
+# print(str(userNumber) + " times 2 is " + str(userNumber * 2))
+# print(str(userNumber) + " times 3 is " + str(userNumber * 3))
+# print(str(userNumber) + " times 4 is " + str(userNumber * 4))
+# print(str(userNumber) + " times 5 is " + str(userNumber * 5))
+# print(str(userNumber) + " times 6 is " + str(userNumber * 6))
+# print(str(userNumber) + " times 7 is " + str(userNumber * 7))
+# print(str(userNumber) + " times 8 is " + str(userNumber * 8))
+# print(str(userNumber) + " times 9 is " + str(userNumber * 9))
+# print(str(userNumber) + " times 10 is " + str(userNumber * 10))
+
+
 userNumber = input("Tell a whole number between 1 and 10: ")
 userNumber = int(userNumber)
+
+for numbers in range(1,11):
+	userNumber * range
+	
+userNumber = str(userNumber)	
+	
 print(userNumber)
-print(str(userNumber) + " times 2 is " + str(userNumber * 2))
-print(str(userNumber) + " times 3 is " + str(userNumber * 3))
-print(str(userNumber) + " times 4 is " + str(userNumber * 4))
-print(str(userNumber) + " times 5 is " + str(userNumber * 5))
-print(str(userNumber) + " times 6 is " + str(userNumber * 6))
-print(str(userNumber) + " times 7 is " + str(userNumber * 7))
-print(str(userNumber) + " times 8 is " + str(userNumber * 8))
-print(str(userNumber) + " times 9 is " + str(userNumber * 9))
-print(str(userNumber) + " times 10 is " + str(userNumber * 10))
