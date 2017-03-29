267d1149c67cba38ce52d51a5e137ec2c07eb96f
Waleed Nisar <waleed24@hotmail.com>
2017-03-29 12:08:45 -0400

    Simple double program

---
+++ practiceOne.py
@@ -0,0 +1,9 @@
+number = input("Tell me a whole number: ")
+
+number = int(number)
+
+finalNumber = number + number
+
+finalNumber = str(finalNumber)
+
+print("Double your number is " + finalNumber)
